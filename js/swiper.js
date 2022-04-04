7d32fcd142c1438ec7777e510ac76501c9b8434d
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-04-04 06:43:59 -0300

    Adicionado Swiper Slider Feijão

--- js/swiper.js
+++ js/swiper.js
@@ -16,3 +16,22 @@ var swiper = new Swiper(".arrox-box", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".feijao-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
