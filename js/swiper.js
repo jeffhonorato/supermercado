b1b5ff74462d8be94f6d9b1aad0fbcd75ecd28e2
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-04-04 07:14:55 -0300

    Swiper Alimentos

--- js/swiper.js
+++ js/swiper.js
@@ -54,3 +54,41 @@ var swiper = new Swiper(".arrox-box", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".frutas-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".enlatados-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
