bed0210ce327fa8dd907ba597991101e856f6ed6
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-04-04 09:47:51 -0300

    Swiper Bebidas - água

---
+++ js/swiper-bebidas.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".bebidas-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
