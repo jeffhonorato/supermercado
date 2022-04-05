a5a4dbcfc2bdf5616129ef25fd1e3f114c98b853
Jefferson Honorato <53011109+jeffhonorato@users.noreply.github.com>
2022-04-05 13:50:40 -0300

    Adicionando Cards - Limpeza Geral

--- js/swiper-limpeza.js
+++ js/swiper-limpeza.js
@@ -35,3 +35,22 @@ var swiper = new Swiper(".limpeza-box", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".limpeza-geral-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
