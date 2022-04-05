294ba65cd8f946c83982554ce927e46ad6e3c0fb
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-04-05 09:36:48 -0300

    Adicionando Bebidas- Cards

--- js/swiper-bebidas.js
+++ js/swiper-bebidas.js
@@ -1,4 +1,42 @@
-var swiper = new Swiper(".bebidas-box", {
+var swiper = new Swiper(".agua-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".refri-box", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".cerveja-box", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
